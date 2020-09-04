a6f228e4c8c0e8e1e3d40231e8390ae0ef96c2d1
pbialecki_augeofi <pbialecki@augeofi.com>
2020-09-03 19:09:29 -0500

    Reorganization and NPM updates.

---
+++ website/gif-search/build/precache-manifest.6e6bd5d48d26e870c9ef7c778fcb79ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6afd55a1e6a9ab943b642205cb8ff2ba",
+    "url": "/github/website/gif-search/index.html"
+  },
+  {
+    "revision": "96b4bf4fc58b1a6d07c9",
+    "url": "/github/website/gif-search/static/css/main.a7be0049.chunk.css"
+  },
+  {
+    "revision": "6e46296e5b7310fc35c8",
+    "url": "/github/website/gif-search/static/js/2.ff600fea.chunk.js"
+  },
+  {
+    "revision": "c3c8fc723dfe96bca9a6ee820a5c1c61",
+    "url": "/github/website/gif-search/static/js/2.ff600fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b4bf4fc58b1a6d07c9",
+    "url": "/github/website/gif-search/static/js/main.868210eb.chunk.js"
+  },
+  {
+    "revision": "2faf1f99f47e172a9e98",
+    "url": "/github/website/gif-search/static/js/runtime-main.30a0b039.js"
+  }
+]);
